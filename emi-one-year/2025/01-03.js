a34562412762706d127e8a2250232449e4b3c95c
Mizar01 <zanarotti.michele@gmail.com>
2024-12-31 14:37:28 +0100

    Added updated version of emi-one-year

---
+++ emi-one-year/2025/01-03.js
@@ -0,0 +1,82 @@
+timedTextsWithFireworks1([
+    {
+        text: "Il settimo giorno Dio creò l'uomo.", 
+        time: 2000,
+    },
+    {
+        text: "L'ottavo giorno l'uomo si lamentò che non aveva un compagno e Dio creò la donna.",
+        time: 4500,
+    },
+    { 
+        text: "Il nono giorno la donna si lamentò e basta.",
+        time: 2500,
+    },
+    { 
+        text: "Il decimo giorno Dio creò:",
+        time : 2500,
+    },
+    {
+        text: "1. L'aspirapolvere",
+        time: 2000,
+    },
+    {
+        text: "2. Il frullatore",
+        time: 2000,
+    },
+    {
+        text: "3. La lavatrice",
+        time: 2500,
+    },
+    {
+        text: "4. Il forno a microonde",
+        time: 2000,
+    },
+    {
+        text: "5. Il robot da cucina",
+        time: 2000,
+    },
+    {
+        text: "6. Il ferro da stiro",
+        time: 2000,
+    },
+    {
+        text: "7. Il tostapane",
+        time: 2000,
+    },
+    {
+        text: "8. La macchina del caffè",
+        time: 2000,
+    },
+    {
+        text: "9. Il bollitore",
+        time: 2000,
+    },
+    {
+        text: "10. Il frigorifero",
+        time: 2000,
+    },
+    {
+        text: "11. Il congelatore",
+        time: 2000,
+    },
+    { 
+        text: "Ma la donna ancora si lamentava.",
+        time: 2500,
+    },
+    { 
+        text: "Allora Dio creò l'arrotino",
+        time: 4000,
+    },
+    {
+        text: "L'uomo allora si lamentò che non aveva niente da fare.",
+        time: 3000,
+    },
+    {
+        text: "Così Dio gli creò la playstation.",
+        time: 3000,
+    },
+    {
+        text: "E la donna e l'arrotino vissero felici e contenti.",
+        time: 3000,
+    }
+]);
