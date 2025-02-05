8af97818c0b280ccf018501513373ad156c5adce
Mizar01 <zanarotti.michele@gmail.com>
2025-02-05 22:15:10 +0100

    Added updated version of emi-one-year

---
+++ emi-one-year/2025/02/02-09.js
@@ -0,0 +1,5 @@
+fireWithAutoTimedText1(`
+    Le nostre virtù e i nostri difetti sono inseparabili, come la forza e la materia. 
+    Quando si separano, l'uomo non c'è più
+    (Nikola Tesla)
+`, 100);
---
+++ emi-one-year/2025/02/02-12.js
@@ -0,0 +1,13 @@
+fireWithAutoTimedText1(`
+    A volte, quando hai una strada molto lunga davanti a te, pensi a quanto è terribilmente lunga e sei sicuro che non riuscirai mai a spazzarla. 
+    E poi inizi ad affrettarti. Lavori sempre più velocemente e ogni volta che alzi lo sguardo sembra che ci sia ancora tanto da spazzare come prima, 
+    e ti impegni ancora di più, e vai nel panico, e alla fine sei senza fiato e devi fermarti, e la strada continua a estendersi davanti a te. 
+    Non è questo il modo di farlo.
+    Non devi mai pensare a tutta la strada in una volta, capito? Devi concentrarti solo sul passo successivo, 
+    sul respiro successivo, sul colpo di scopa successivo, e quello dopo, e quello dopo ancora. Nient'altro.
+    In questo modo ti godi il tuo lavoro, il che è importante, perché poi fai un buon lavoro. 
+    Ed è così che dovrebbe essere.
+    E all'improvviso, prima che tu te ne accorga, scopri di aver spazzato via tutta la strada, un pezzo alla volta. 
+    E per giunta, non sei senza fiato. Questo è importante...
+    (Michael Ende)
+`, 100);
