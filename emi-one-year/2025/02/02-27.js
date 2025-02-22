bce300c2648a9954d1d09ddecbb1fa63db93690f
Mizar01 <zanarotti.michele@gmail.com>
2025-02-22 16:44:54 +0100

    Added updated version of emi-one-year

---
+++ emi-one-year/2025/02/02-27.js
@@ -0,0 +1,13 @@
+fireWithAutoTimedText1(`
+    Una scintilla, un barlume di luce,
+    s'accende nella notte,
+    la notte della nostra tristezza.
+    Quando ci ricordiamo tutte le cose
+    che amiamo di noi
+    E poi ci rendiamo conto
+    che siamo noi a creare luce attorno
+    solo noi
+    noi
+    (ROIMETIC HAZELANT)
+     
+`, 100);
