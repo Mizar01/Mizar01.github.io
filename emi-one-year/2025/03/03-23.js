c24c43448de07febacdf44ff80076009fbcbc87e
Mizar01 <zanarotti.michele@gmail.com>
2025-03-21 17:01:24 +0100

    Added updated version of emi-one-year

---
+++ emi-one-year/2025/03/03-23.js
@@ -0,0 +1,23 @@
+createPhaserGame(_create);
+
+function _create() {
+
+    setBgColor("#000000");
+
+    createSnowflakes2(0x0000aa);
+
+    autoTimedTexts(`
+        Strufi e il bosco stregato
+        Capitolo 6**
+        *
+        Le cascate dell’oblio erano maestose. Strufi ci passò accanto notando che anche in quella zona tutti erano spariti. 
+        Una piccola farfalla dorata volò proprio accanto a lui. Strufi la segui, e finì per trovarsi proprio dietro le scroscianti cascate.
+        Seguì la farfalla percorrendo una sottile sporgenza lungo le pareti, con sotto un terribile strapiombo turbinante di acqua, 
+        finché giunse ad una vera e propria grotta. 
+        La grotta fù illuminata dai primi raggi del sole dell’alba, filtrati e mitigati dal muro d’acqua.
+        “Chi è che osa disturbarmi?” 
+        Una voce oscura e profonda fece rabbrividire Strufi.
+
+    `);
+
+}
--- emi-one-year/tags.js
+++ emi-one-year/tags.js
@@ -34,4 +34,5 @@ const tagsPerDay = [
     {date: "2025-03-18", tags: ["wedding"]},
     {date: "2025-03-21", tags: ["songs"]},
     {date: "2025-03-22", tags: ["quiz"]},
+    {date: "2025-03-23", tags: ["strufi"]},
 ]
