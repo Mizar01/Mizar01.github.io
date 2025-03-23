92b67b3d72728812c05ed1ac5003b18abd447b45
Mizar01 <zanarotti.michele@gmail.com>
2025-03-23 18:44:22 +0100

    Added updated version of emi-one-year

---
+++ emi-one-year/2025/03/03-24.js
@@ -0,0 +1,6 @@
+fireWithAutoTimedText1(`
+    L’amore è l’arte di stare con gli altri, 
+    la meditazione è l’arte di essere in relazione con se stessi, 
+    lascia che l’amore e la meditazione siano le tue due ali.
+    (Osho)
+`, 100);
---
+++ emi-one-year/2025/03/03-25.js
@@ -0,0 +1,4 @@
+fireWithAutoTimedText1(`
+    La meditazione è una macchina del tempo per entrare più in profondità nel momento presente.
+    (Fabrizio Caramagna)
+`, 100);
