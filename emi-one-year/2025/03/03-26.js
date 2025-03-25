b7123b9f212b54ad513303723d65fdc7983d7657
Mizar01 <zanarotti.michele@gmail.com>
2025-03-25 16:52:14 +0100

    Added updated version of emi-one-year

---
+++ emi-one-year/2025/03/03-26.js
@@ -0,0 +1,24 @@
+snowFlakesWithAutoTimedText1("😆",`
+    L'ANGOLO DEL BUON UMORE**
+    CHE TANTO PIACE ALLA BUMBI*
+    *
+    L'inventore della BIC si è gettato dalla finestra.
+    Ci ha rimesso le penne.
+    *
+    Qual è il colmo per un pizzaiolo? 
+    Avere una Diavola di figlia che fa la Capricciosa per 4 Stagioni.
+    *
+    (Strano il colmo di prima, perché Pietro non è pizzaiolo.)
+    *
+    Cosa fa un gondoliere quando si annoia alla tv?
+    Cambia canale.
+    *
+    Qual è il colmo per un gorilla?
+    Entrare nel Guinness dei Primati
+    *
+    Qual'è il latte preferito di un tossico?
+    Il latte da vena.
+`, {
+    ratio: 100, 
+    fill: "#ffffff",
+});
