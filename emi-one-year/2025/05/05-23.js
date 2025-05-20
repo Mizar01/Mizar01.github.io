4c924b75d65c85847315f5f550b96427a4257b94
Mizar01 <zanarotti.michele@gmail.com>
2025-05-20 14:16:58 +0200

    Added updated version of emi-one-year

---
+++ emi-one-year/2025/05/05-23.js
@@ -0,0 +1,29 @@
+createPhaserGame(_create);
+
+function _create() {
+
+    setBgColor("#000000");
+
+    createSnowflakes2(0x0000aa);
+
+    autoTimedTexts(`
+        Shamana Pigiama e il tortellino nero
+        CAPITOLO 4
+        Alla fine della scala a chiocciola, Shamana arrivò in un piccolo antro scavato nella terra, tutto polveroso che la fece tossire. 
+        Grubenda la veggente era intenta ad aprire il lucchetto di un piccolo scrigno ricoperto di pietre colorate. 
+        Quando lo aprì un fumo scuro uscì e poco dopo tirò fuori un piccolo tortellino nero.
+        “Questo ti aiuterà!” disse la vecchia.
+        “Che cos’è?” chiese Shamana eccitata.
+        “Non lo vedi? È un tortellino nero, tu ragazza, hai dei poteri speciali che non tutti hanno, e questo piccolo tortellino artigianale può risvegliare la tua vera identità, e darti finalmente tutte le risposte che cerchi”.
+        “Anche sul futuro?” chiese Shamana. “Ecchenesò io! Ti darà delle risposte, ecco, mettitelo al dito”.
+        Il tortellino si adatto perfettamente all’indice di Shamana, la quale iniziò ad avvertire una sensazione di freddo, come se le avessero avvolto al dito un cubo di ghiaccio.
+        “Lascia che il tortellino si abitui al tuo corpo, e vedrai i risultati. Ora devo lasciarti. Non dimenticare di chiudere la porta della roulotte quando esci”
+        “Ma io…”
+        “Non posso fare altro per te. Consideralo un regalo, mi ringrazierai un giorno, se vorrai” e Grubenda sparì nell’oscurita di un corridoio scavato nella terra.
+        Shamana uscì dalla roulotte e se ne tornò a casa fissando il tortellino per tutto il tempo.
+        Alla fine del lungo tunnel Grubenda giunse ad un grosso pentolone, circondato dai suoi servitori che vi gettavano piccoli animaletti di ogni tipo. Il liquido verdastro del pentolone ribolliva.
+        Grubenda si levò la parrucca di capelli ricci ed esclamò: “Ah ah ah! Shamana è caduta ‘nuovamente’ nella mia trappola. 
+        Aveva smesso di mangiare il krapfen Wendi, il mio primo veleno, ma ora posso sistemare le cose grazie al tortellino nero!”
+    `);
+
+}
---
+++ emi-one-year/2025/05/05-25.js
@@ -0,0 +1,4 @@
+fireWithAutoTimedText1(`
+    Il mio scopo nella vita è far pensare la gente. Se ci riesco, ho avuto successo.
+    (Charles A. Reich)
+`, 100);
