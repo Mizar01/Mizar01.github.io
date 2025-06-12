219df129546e75611df2ef23c4821c38ee2608cd
Mizar01 <zanarotti.michele@gmail.com>
2025-06-12 08:48:56 +0200

    Added updated version of emi-one-year

---
+++ emi-one-year/2025/06/06-01.js
@@ -0,0 +1,20 @@
+snowFlakesWithAutoTimedText1("🎬", `
+    I MIGLIORI FILM SOLO PER TE (versione xxx)**
+    *
+    TRAILER 1**
+    Una leggiadra e innocente fanciulla fuggita nel bosco incontra sette piccoletti superdotati che le danno alloggio in cambio di servigi particolari.
+    *
+    "Biancaneve sotto i nani"**
+    TRAILER 2**
+    Un vecchio mago del castello addestra il suo pupillo Artù a diventare uomo, partendo in modo molto brusco.
+    *
+    "La spada nella doccia"**
+    TRAILER 3**
+    Non perdete il prossimo film di Almodovar, dove una madre ricorda i momenti passati con il figlio, e con i suoi numerosi amanti.
+    *
+    Non perdete "Tutti su mia madre"**
+    TRAILER 4**
+    Un gruppo di onanisti patologici, stigmatizzati dalla società, si rifugiano nel nautilus, un sottomarino che può raggiungere le profondità più oscure.
+    *
+    Venite a vedere "20.000 seghe sotto i mari"**
+`, 80);
---
+++ emi-one-year/2025/06/06-15.js
@@ -0,0 +1,21 @@
+snowFlakesWithAutoTimedText1("😆",`
+    L'ANGOLO DEL BUON UMORE**
+    *
+    La canzone preferita da un elettricista?
+    "Questione di filiiii!"
+    *
+    Perché i fantasmi non possono mentire?
+    Perché si vedono attraverso!
+    *
+    Perché il re è nudo ma nessuno lo può dire?
+    Perché... ehi... ehi... che fate? No stavo scherzando, non è nudo, si vede benissimo che ha un completino rosa, no, aiuto, la ghigliottina no!
+    *
+    Quando un uomo entra in un caffé fa "splash!"
+    Quando una donna entra in un caffé fa "Buongiorno, un caffé per favore!".
+    *
+    Un fantino molto triste dice di non poter pulire i denti al suo cavallo
+    perché era un regalo. Maledetti proverbi!
+`, {
+    ratio: 100, 
+    fill: "#ffffff",
+});
