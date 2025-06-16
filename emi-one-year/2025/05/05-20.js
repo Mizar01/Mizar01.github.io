67c7859cd46eb6f1568acbe44c148a83c13e9ff0
Mizar01 <zanarotti.michele@gmail.com>
2025-06-16 09:02:49 +0200

    Added updated version of emi-one-year

--- emi-one-year/2025/05/05-20.js
+++ emi-one-year/2025/05/05-20.js
@@ -1,7 +1,20 @@
 fireWithAutoTimedText1(`
-    Parti con un pianeta come la Terra, 
-    con un insieme di composti semplici destinati ad esistere su di esso, 
-    aggiungi l'energia di un sole vicino e sei destinato a finire con acidi nucleici. 
-    Non puoi evitarlo.
-(Isaac Asimov)
+    «S'i' fosse foco, ardere' il mondo;
+    s'i' fosse vento, lo tempestarei;
+    s'i' fosse acqua, i' l'annegherei;
+    s'i' fosse Dio, mandereil'en profondo;
+
+    s'i' fosse papa, serei allor giocondo,
+    ché tutti cristïani embrigarei;
+    s'i' fosse 'mperator, sa' che farei?
+    a tutti mozzarei lo capo a tondo.
+
+    S'i' fosse morte, andarei da mio padre;
+    s'i' fosse vita, fuggirei da lui:
+    similemente faria da mi' madre,
+
+    S'i' fosse Cecco, com'i' sono e fui,
+    torrei le donne giovani e leggiadre:
+    le vecchie e laide lasserei altrui.»
+    (Cecco Angiolieri)
 `, 100);
---
+++ emi-one-year/2025/06/06-19.js
@@ -0,0 +1,7 @@
+fireWithAutoTimedText1(`
+    In realtà ci sono tante religioni quante sono le persone... 
+    Le religioni sono strade diverse che convergono nello stesso punto. 
+    Che importa se prendiamo strade diverse, purché raggiungiamo lo stesso obiettivo? 
+    Qual è la causa per litigare?
+    (Mahatma Gandhi)
+`, 100);
