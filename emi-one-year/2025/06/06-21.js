f3610022e7d7e04697a11992d4f45f8e94ddac6b
Mizar01 <zanarotti.michele@gmail.com>
2025-06-17 08:26:46 +0200

    Added updated version of emi-one-year

---
+++ emi-one-year/2025/06/06-21.js
@@ -0,0 +1,4 @@
+fireWithAutoTimedText1(`
+    È il più grande di tutti gli errori non fare nulla col motivo che puoi fare poco.
+    (Sidney Smith)
+`, 100);
---
+++ emi-one-year/2025/06/06-22.js
@@ -0,0 +1,11 @@
+snowFlakesWithManualText1("?", `
+    FRASI CELEBRI - INDOVINA IL FILM
+    Regola uno: niente minerali o vegetali, solo animali. Regola due: non cose immaginarie come... draghi verdi e roba simile. Regola tre: non scomparire! 
+    (Maga Magò - La spada nella roccia)
+    Ho colpito i tōi, due punti dei 708 punti segreti di pressione. Quando estrarrò i miei pollici, ti rimarranno tre secondi di vita.
+    (Ken il Guerriero)
+    I popoli non dovrebbero avere paura dei propri governi, sono i governi che dovrebbero aver paura dei popoli.
+    (V for Vendetta)
+    Tu ce l'hai mai avuta una corda al collo? Be', t'assicuro che quando si mette a stringere tu senti già il diavolo che ti morde le chiappe.
+    (Il buono, il brutto, il cattivo)
+`, 100);
