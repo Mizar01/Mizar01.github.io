c9fa076b98abe2d229deccb73d7ca0b4f27d4a8d
Mizar01 <zanarotti.michele@gmail.com>
2025-06-23 08:05:31 +0200

    Added updated version of emi-one-year

---
+++ emi-one-year/2025/06/06-24.js
@@ -0,0 +1,12 @@
+fireWithAutoTimedText1(`
+    Anche una giornata di sole cocente, 
+    con caldo afoso, 
+    con sabbia che ti entra nelle viscere,
+    e conchiglie spezzate che ti tagliano i piedi,
+    con piadine dal prosciutto avariato,
+    e la cui fontina ti provoca lo squaraus,
+    può essere recuperata dal tuo sorriso,
+    dal tuo viso con le gote rosse,
+    i tuoi occhi lucidi e stanchi,
+    ma sorridenti anch'essi.
+`, 100);
