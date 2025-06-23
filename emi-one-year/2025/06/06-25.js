8249bd9168ff1c1a887a8b266d849cf9cff93c70
Mizar01 <zanarotti.michele@gmail.com>
2025-06-23 08:07:46 +0200

    Added updated version of emi-one-year

---
+++ emi-one-year/2025/06/06-25.js
@@ -0,0 +1,10 @@
+snowFlakesWithManualText1("?", `
+    I PENSIERI FUGACI DEL BUMBI
+    - Il mare che palleeeeeee!
+    - Il sole che palleeeeeee!
+    - La sabbia che palleeeeeee!
+    - Perché non sappiamo mai deciderci?
+    - Perché ogni posto deve avere un problema? Eh Michele? Perché?
+    - Perché non si può stare a casa sul divano a fissare la tv spenta?
+    - Non voglio toccare il fiume marron coi piedi nudi!
+`, 100);
