b40b5d35aa0ae534c4c31cdea379519777a09dec
Mizar01 <zanarotti.michele@gmail.com>
2025-06-24 08:53:01 +0200

    Added updated version of emi-one-year

---
+++ emi-one-year/2025/06/06-26.js
@@ -0,0 +1,19 @@
+snowFlakesWithAutoTimedText1("😆",`
+    L'ANGOLO DEL BUON UMORE**
+    CHE TUTTO IL MONDO CI INVIDIA*
+    *
+    Che cosa fa il mare a bordo di una nave?
+    Si agita!
+    *
+    Che cosa fa il sole quando si arrabbia?
+    Si infervora!
+    *
+    Che cosa fa la sabbia quando è felice?
+    Si spiaggia!
+    *
+    Come si fa a capire se una mosca scoreggia?
+    Per qualche attimo vedi che va in rettilineo!
+`, {
+    ratio: 100, 
+    fill: "#ffffff",
+});
---
+++ emi-one-year/2025/06/06-27.js
@@ -0,0 +1,7 @@
+fireWithAutoTimedText1(`
+Quindi eccoti! Avrei dovuto sapere che sarebbe finita così. 
+Pensare a tutto l'amore e l'affetto che ti ho dedicato. 
+Beh, se è così che tratti la tua povera vecchia madre nell'autunno della sua vita, 
+tutto quello che posso dire è: "Vai avanti. Fatti crocifiggere. Sai quanto me ne frega."
+(Madre di Brian di Nazareth - Monty Python's Life of Brian)
+`, 100);
