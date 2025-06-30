3bb9c6a2a87d92274377a684e724fe70872b5c6c
Mizar01 <zanarotti.michele@gmail.com>
2025-06-30 16:47:26 +0200

    Added updated version of emi-one-year

---
+++ emi-one-year/2025/07/07-01.js
@@ -0,0 +1,20 @@
+snowFlakesWithAutoTimedText1("🎬", `
+    I MIGLIORI FILM SOLO PER TE**
+    *
+    TRAILER 1**
+    Un uomo incontra una donna, l'amore sembra sbocciare, le labbra si incontrano,
+    ma la cena di quella sera provocò qualcosa di inaspettato proprio in quel momento.
+    *
+    Non perdetevi "Pensavo fosse amore, invece era uno squaraus"**
+    TRAILER 2**
+    Una donnina e un omino si incontravano in un barettino segreto, era il loro nascondiglino,
+    nessuno lo conosceva, e si davano tanti bacini, univano le manine e le gambine.
+    *
+    Non perdetevi "Il postino"**
+    TRAILER 3**
+    Un vecchietto riceve dal suo amico in regalo un piccolo insetto, lo adotta e gli insegna a parlare
+    Purtroppo molto presto il piccolo insetto impara anche a dire le bugie, e questo lo porterà a vivere
+    una drammatica e disperata avventura.
+    *
+    Non perdete "Le avventure di Pidocchio"** in sconto a 19.99 euro allo Space.
+`, 80);
